3afc767b2b0093e724248fb1e34c295b45ad0b19
ashrafulislam5158@gmail.com <ashrafulislam5158@gmail.com>
2023-08-04 10:24:16 +0600

    popup multi done

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const popupImgElm = document.querySelector(".popup_img")
+const popupImgElm = Array.from(document.querySelectorAll(".popup_img"))
 // console.log(popupImgElm.children[0].children[0])
 const popupModalElm = document.querySelector(".popup_modal")
 const popupModalImgElm = document.querySelector(".popup_modal_img")
 
-popupImgElm.addEventListener("click", () => {
-  popupModalElm.style.display = "flex"
-  popupModalImgElm.style.transform = "scale(1.8)"
-})
+
 const closeElm = document.querySelector(".close")
 closeElm.addEventListener("click",()=>{
   popupModalElm.style.display = "none"
+})
+
+popupImgElm.map((item) => {
+  item.addEventListener("click", () => {
+    let imgsrc = item.children[0].children[0].getAttribute("src")
+    popupModalImgElm.setAttribute("src",imgsrc);
+    popupModalElm.style.display = "flex"
+    popupModalImgElm.style.transform = "scale(1.8)"
+  })
+
 })
